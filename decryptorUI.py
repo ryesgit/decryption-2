944a3ca2236021832460562621b78b6057a9fde2
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-04-17 14:11:32 +0800

    Define user interface layout

---
+++ decryptorUI.py
@@ -0,0 +1,25 @@
+'''
+Filename: DecryptorUI.py
+The User Interface for The Decryptor
+'''
+
+import PySimpleGUI as sg
+
+sg.theme('DarkAmber')
+
+layout = [
+    [sg.Text('', size=(20,1)), sg.Text('THE DECRYPTOR', font=('Any 20'), auto_size_text=True, justification='center'), sg.Text('', size=(20,1))],
+    [sg.Text('Enter text to decrypt: '), sg.InputText(key='input')],
+    [sg.Text('Decrypted Text: '), sg.Text(key='output', font=('Roboto', 14), background_color='black')],
+    [sg.Button('Decrypt', bind_return_key=True), sg.Button('Quit')]
+]
+
+window = sg.Window('Decryptor', layout)
+
+while True:
+    event, values = window.read()
+
+    if (event == sg.WIN_CLOSED or event == 'Quit'):
+        break
+
+window.close()
